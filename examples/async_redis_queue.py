10fb10f4d20c288ccb6e1ff2712e6df780e870d1
rednafi <redowan.nafi@gmail.com>
2021-05-16 03:31:26 +0600

    Finalized example of using redis queue as a mean of interprocess com

--- examples/async_redis_queue.py
+++ examples/async_redis_queue.py
@@ -5,13 +5,9 @@ Description
 ===========
 
 This script implements a rudimentary FIFO task queue using Redis's list data
-structure. I took a peek under Celery and RQ's source code to understand how
-they've implemented the async task queue — harnessing the features of Redis and
-Python's multiprocessing paradigm.
+structure.
 
-Here, I've attempted to emulate a similar task queue — albeit in a much simpler
-fashion — to strengthen my grasp on the concept of async task queue. This script
-roughly implements the following steps:
+This script roughly implements the following steps:
 
 -> Task callables are turned into task objects.
 
@@ -22,10 +18,10 @@ roughly implements the following steps:
 
 -> Broker stores the tasks in a FIFO queue.
 
--> A worker runs 4 OS processes that listen to the broker database to find tasks.
+-> A single worker picks up the tasks.
 
 -> When a task is found by the worker, it pops that from the FIFO queue, performs
-   deserialization, and executes it.
+   deserialization, and executes it in a non-blocking fashion.
 
 -> The worker sends the task result to a result backend which is just another
    Redis database.
@@ -73,8 +69,7 @@ import logging
 import pickle
 import random
 import uuid
-from dataclasses import dataclass
-from typing import Any, Callable, Coroutine, Iterator
+from typing import Any, Callable, Coroutine
 
 import aioredis
 import uvloop
@@ -82,40 +77,7 @@ from aioredis.client import Redis
 
 logging.basicConfig(level=logging.INFO)
 
-MAX_WORKERS = 4
 uvloop.install()
-
-
-@dataclass
-class Config:
-    broker_url: str
-    result_backend_url: str
-    task_map: dict[str, Iterator[Callable[[Any], Any]]]
-
-    @property
-    def _broker(self) -> Redis:
-
-        return aioredis.from_url(self.broker_url)
-
-    @property
-    def _result_backend(self) -> Redis:
-        return aioredis.from_url(self.result_backend_url)
-
-    def distill(self) -> None:
-        """Sanitizes the inputs."""
-        if not isinstance(self.broker_url, str):
-            raise TypeError("`broker_url` needs to be a string")
-
-        if not isinstance(self.result_backend_url, str):
-            raise TypeError("`result_backend_url` needs to be a string")
-
-        if not isinstance(self.task_map, dict):
-            raise TypeError(
-                "`task_map` needs to be a dict where the key is the queue name and the value is a tuple of coroutine functions."
-            )
-
-    def __post_init__(self) -> None:
-        self.distill()
 
 
 class SimpleTask:
@@ -174,29 +136,14 @@ class Queue:
         result = await task.process_task()
 
         # Save the result using Redis's `key:val` structure.
-        await self.result_backend.set(f"{task.id}", result)
+        await self.result_backend.set(f"result:{task.id}", result)
         logging.info("Task processing complete.")
 
-    async def get_length(self) -> int:
+    async def get_length(self) -> Coroutine[None, None, int]:
         return await self.broker.llen(self.queue_name)
 
 
-class Orchestrator:
-    def __init__(self, config: Config) -> None:
-        self.config = config
-
-    async def enqueue_all(self, args: tuple[tuple, ...]) -> None:
-        for queue_name, funcs in self.config.task_map.items():
-            self.queue = Queue(
-                self.config._broker,
-                self.config._result_backend,
-                queue_name,
-            )
-            for func, arg in zip(funcs, args):
-                await self.queue.enqueue(func, *arg)
-
-
-async def worker(queue: Queue, max_worker: int = MAX_WORKERS) -> None:
+async def worker(queue: Queue) -> None:
     """Mimicks the celery worker."""
 
     async def _execute_task(queue: Queue) -> None:
@@ -204,40 +151,35 @@ async def worker(queue: Queue, max_worker: int = MAX_WORKERS) -> None:
             await queue.dequeue()
         else:
             logging.info("No tasks in the queue")
+            await asyncio.sleep(0)
 
-    logging.info(f"Running tasks with {max_worker} processes!")
-
-    background_tasks = [
-        asyncio.create_task(_execute_task(queue))
-        for _ in range(await queue.get_length())
-    ]
-
-    await asyncio.gather(*background_tasks)
+    for _ in range(await queue.get_length()):
+        await _execute_task(queue)
 
 
 # Define a task to be run asynchronously.
-async def task(start: int, end: int) -> int:
-    # await asyncio.sleep(0)
+async def foo(start: int, end: int) -> int:
+    await asyncio.sleep(0)
     return random.randint(start, end)
 
 
 async def main():
     # Instantiate Redis `broker` and `result_backend` connections.
-    task_map = {"default": (task for _ in range(1000))}
-    args = tuple((1000, 2000) for _ in range(1000))
+    broker = aioredis.from_url("redis://localhost:6379/0")
+    result_backend = aioredis.from_url("redis://localhost:6379/1")
 
-    config = Config(
-        broker_url="redis://localhost:6379/0",
-        result_backend_url="redis://localhost:6379/0",
-        task_map=task_map,
-    )
+    async with broker.client() as b:
+        async with result_backend.client() as rb:
+            queue = Queue(b, rb, "default")
+            funcs = [foo for _ in range(1000)]
+            args = [(i, j) for (i, j) in zip(range(1000), range(1000, 2000))]
 
-    orch = Orchestrator(config)
-    await orch.enqueue_all(args)
+            # Enqueue tasks.
+            for func, arg in zip(funcs, args):
+                print("enqueing task")
+                await queue.enqueue(func, *arg)
 
-    # Spawn 4 parallel processes, fetch tasks from the queue and execute them
-    # asynchronously.
-    await worker(orch.queue)
+            await worker(queue)
 
 
-asyncio.run(main())
+asyncio.run(main(), debug=True)
