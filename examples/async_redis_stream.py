84cbfcca1c6951fa1acc9103fb7ebe4d6f139116
rednafi <redowan.nafi@gmail.com>
2021-05-21 05:27:38 +0600

    redis streams multiple stream producer consumer

--- examples/async_redis_stream.py
+++ examples/async_redis_stream.py
@@ -21,6 +21,17 @@ STREAM_NAME = "dhaka"
 STREAM_MAP = {"dhaka": "$"}
 CONSUMER_GROUP_NAME = "demo_consumer"
 
+# Create consumer group.
+async def create_consumer_group(
+    stream_name: str = STREAM_NAME,
+    consumer_group_name: str = CONSUMER_GROUP_NAME,
+) -> None:
+    async with REDIS_POOL.client() as conn:
+        try:
+            await conn.xgroup_create(stream_name, consumer_group_name)
+        except:
+            pass
+
 
 async def producer(
     redis_pool: Redis = REDIS_POOL,
@@ -43,21 +54,16 @@ async def consumer(
     redis_pool: Redis = REDIS_POOL,
     stream_name: str = STREAM_NAME,
     stream_map: dict[str, str] = STREAM_MAP,
-    consumer_group: str = CONSUMER_GROUP_NAME,
+    consumer_group_name: str = CONSUMER_GROUP_NAME,
 ) -> None:
 
     async with redis_pool.client() as conn:
-        # Create consumer group.
-        try:
-            await conn.xgroup_create(stream_name, consumer_group)
-        except:
-            pass
 
         while True:
             result = await conn.xread(stream_map, block=1)
             if result:
                 stream_id = result[0][1][0][0]
-                await conn.xack(stream_name, consumer_group, stream_id)
+                await conn.xack(stream_name, consumer_group_name, stream_id)
                 print(f"consumer ingested data :{result}")
                 await asyncio.sleep(1)
             else:
@@ -65,7 +71,20 @@ async def consumer(
 
 
 async def orchestrator() -> None:
-    task_coros = (producer(), consumer(), consumer())
+    await create_consumer_group()
+    await create_consumer_group(
+        stream_name="chittagong",
+        consumer_group_name="another",
+    )
+    task_coros = (
+        producer(),
+        producer(stream_name="chittagong"),
+        consumer(),
+        consumer(
+            stream_name="chittagong",
+            stream_map={"chittagong": "$"},
+        ),
+    )
     await asyncio.gather(*task_coros)
 
 
