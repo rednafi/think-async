ae6e28ce9535356b248d01ee1d6d601acb2aa21d
redowan <redowan.nafi@gmail.com>
2021-11-04 04:46:05 +0600

    Added test for limit_coroutine_execution

--- patterns/limit_coroutine_execution.py
+++ patterns/limit_coroutine_execution.py
@@ -8,14 +8,15 @@ async def echo(term: str, limit: asyncio.Semaphore) -> None:
         print(term)
         await asyncio.sleep(0.1)
         if limit.locked():
-            print(f"limit crossed, sleeping for {2} seconds")
+            print("limit crossed, sleeping for 2 seconds")
             await asyncio.sleep(2)
 
 
-async def main() -> None:
+async def orchestrator() -> None:
     limit = asyncio.Semaphore(3)  # type: asyncio.Semaphore
     consumers = [echo("Semaphore is awesome!", limit) for _ in range(9)]
     await asyncio.gather(*consumers)
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(orchestrator())
---
+++ tests/test_limit_coroutine_execution.py
@@ -0,0 +1,43 @@
+import asyncio
+from unittest.mock import patch
+
+import pytest
+
+import patterns.limit_coroutine_execution as main
+
+
+@pytest.mark.asyncio
+@patch("patterns.limit_coroutine_execution.asyncio.sleep", autospec=True)
+async def test_echo(mock_asyncio_sleep, capsys):
+    limit = asyncio.Semaphore(1)
+
+    # Call 'echo'.
+    for _ in range(3):
+        await main.echo(term="testing echo", limit=limit)
+
+    # Assert.
+    out, err = capsys.readouterr()
+    assert err == ""
+    assert "limit crossed" in out
+    mock_asyncio_sleep.assert_awaited()
+
+
+@pytest.mark.asyncio
+@patch(
+    "patterns.limit_coroutine_execution.asyncio.Semaphore",
+    autospec=True,
+    return_value=asyncio.Semaphore(1),
+)
+@patch("patterns.limit_coroutine_execution.asyncio.sleep", autospec=True)
+async def test_orchestrator(mock_asyncio_sleep, mock_asyncio_semaphore, capsys):
+
+    # Call 'orchestrator'.
+    await main.orchestrator()
+
+    # Assert.
+    out, err = capsys.readouterr()
+    assert err == ""
+    assert "Semaphore is awesome" in out
+    assert "limit crossed" in out
+    mock_asyncio_sleep.assert_awaited()
+    mock_asyncio_semaphore.assert_called_once()
